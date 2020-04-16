5573a40f733d3d3e15f30f19e525ac71d4202404
Samuel Huet <contact@huetsamuel.fr>
2020-04-16 12:49:54 +0200

    working with gazebo but bad conversion
    Signed-off-by: Samuel Huet <contact@huetsamuel.fr>

--- scripts/getlidar.py
+++ scripts/getlidar.py
@@ -3,29 +3,14 @@ import random
 from PIL import Image
 import numpy as np
 import rospy
+from sensor_msgs.msg import LaserScan
 
 
 class Sonar:
 
-    def __init__(self):
-        self.lidar_data = []
+    def __init__(self, data):
+        self.lidar_data = data
         self.img = None
-
-    def listener(self):
-        rospy.init_node('listenerLidar', anonymous=True)
-        sub = rospy.Subscriber('/scan', LaserScan, self.callback)
-        # spin() simply keeps python from exiting until this node is stopped
-        rospy.spin()
-
-    def callback(msg):
-        sonar = Sonar()
-        sonar.create_template()
-        # sonar.generate_fake_data()
-        x, y = sonar.to_polar()
-        x, y = sonar.round(x, y)
-        sonar.draw_points(x, y)
-        # toto.show()
-        sonar.saveimg()
 
     def generate_fake_data(self):
         self.lidar_data = [random.randint(0, 10) for i in range(360)]
@@ -41,8 +26,8 @@ class Sonar:
         lidar_data_x = []
         lidar_data_y = []
         for i in range(360):
-            lidar_data_x.append((self.lidar_data[i]*20 * np.cos(i)) + 250)
-            lidar_data_y.append((self.lidar_data[i]*20 * np.sin(i)) + 250)
+            lidar_data_x.append((self.lidar_data[i]*80 * np.cos(i)) + 250)
+            lidar_data_y.append((self.lidar_data[i]*80 * np.sin(i)) + 250)
         return lidar_data_x, lidar_data_y
 
     def draw_points(self, lidar_data_x, lidar_data_y):
@@ -50,18 +35,40 @@ class Sonar:
             self.img.paste((0, 0, 0), (lidar_data_x[i]-2, lidar_data_y[i]-2,
                                        lidar_data_x[i]+2, lidar_data_y[i]+2))
 
-    def round(self, lidar_data_x_old, lidar_data_y_old):
+    def round_data(self, lidar_data_x_old, lidar_data_y_old):
         lidar_data_x = []
         lidar_data_y = []
         for i in range(360):
-            lidar_data_x.append(int(round(lidar_data_x_old[i])))
-            lidar_data_y.append(int(round(lidar_data_y_old[i])))
+            # print(lidar_data_x_old[i])
+            # print(lidar_data_y_old[i])
+            if np.isinf(lidar_data_x_old[i]):
+                lidar_data_x.append(0)
+                lidar_data_y.append(0)
+            else:
+                lidar_data_x.append(int(round(lidar_data_x_old[i])))
+                lidar_data_y.append(int(round(lidar_data_y_old[i])))
         return lidar_data_x, lidar_data_y
 
     def saveimg(self):
         self.img.save("LIDAR.PNG")
 
+def callback(msg):
+    # print(len(msg.ranges))
+    sonar = Sonar(msg.ranges)
+    sonar.create_template()
+    # sonar.generate_fake_data()
+    x, y = sonar.to_polar()
+    x, y = sonar.round_data(x, y)
+    sonar.draw_points(x, y)
+    # toto.show()
+    sonar.saveimg()
+    rospy.loginfo(rospy.get_caller_id() + "   Image created")
+
+def listener():
+    rospy.init_node('listenerLidar', anonymous=True)
+    sub = rospy.Subscriber('/scan', LaserScan, callback)
+    # spin() simply keeps python from exiting until this node is stopped
+    rospy.spin()
 
 if __name__ == '__main__':
-    generate_images = Sonar()
-    generate_images.listener()
+    listener()
